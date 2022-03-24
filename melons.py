c07bd1f94cb05610b920939a278d8c539d736258
Kaitlin Rosen <kayejrosen@gmail.com>
2022-03-24 15:54:57 -0700

    Added abstract Melon Order and inherited into domestic and international order

--- melons.py
+++ melons.py
@@ -3,6 +3,10 @@
 class AbstractMelonOrder:
 
     def __init__(self, species, qty, country_code = "USA"):
+        self.species = species
+        self.qty = qty
+        self.shipped = False
+        self.country_code = country_code
 
     def get_total(self):
         """Calculate price, including tax."""
@@ -18,46 +22,18 @@ class AbstractMelonOrder:
         self.shipped = True
 
 
-class DomesticMelonOrder:
+class DomesticMelonOrder(AbstractMelonOrder):
     """A melon order within the USA."""
 
-    def __init__(self, species, qty):
-        """Initialize melon order attributes."""
-
-        self.species = species
-        self.qty = qty
-        self.shipped = False
-        self.order_type = "domestic"
-        self.tax = 0.08
-
-    
+    tax = 0.8
+    shipping_type = "domestic"
 
 
-class InternationalMelonOrder:
+class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
 
-    def __init__(self, species, qty, country_code):
-        """Initialize melon order attributes."""
-
-        self.species = species
-        self.qty = qty
-        self.country_code = country_code
-        self.shipped = False
-        self.order_type = "international"
-        self.tax = 0.17
-
-    # def get_total(self):
-    #     """Calculate price, including tax."""
-
-    #     base_price = 5
-    #     total = (1 + self.tax) * self.qty * base_price
-
-    #     return total
-
-    # def mark_shipped(self):
-    #     """Record the fact than an order has been shipped."""
-
-    #     self.shipped = True
+    tax = 0.17
+    shipping_type = "International"
 
     def get_country_code(self):
         """Return the country code."""
